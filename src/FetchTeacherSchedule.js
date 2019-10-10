85c396c2fc09ff23f50b0ad409ccd6cf406a116c
abudimirov <alexys077@gmail.com>
2019-10-10 14:09:10 +0300

    added parser for lessons

--- src/FetchTeacherSchedule.js
+++ src/FetchTeacherSchedule.js
@@ -7,8 +7,10 @@ export default class FetchTeacherSchedule extends React.Component {
     state = {
         loading: true,
         person: null,
+        lessons: null,
         week: null,
-        days: null
+        days: null,
+        groups: null
     };
 
 
@@ -17,13 +19,16 @@ export default class FetchTeacherSchedule extends React.Component {
         const url = `https://ruz.spbstu.ru/api/v1/ruz/teachers/${teacher_id}/scheduler`;
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({ schedule: data.days, lessons: data.days.lessons, person: data.teacher, week: data.week, days: data.days, loading: false });
+        this.setState({
+            schedule: data.days,
+            person: data.teacher,
+            week: data.week,
+            days: data.days,
+            loading: false
+        });
         console.log(this.state.schedule);
-        console.log(this.state.schedule[0].lessons);
-
+        console.log(this.state.lessons);
     }
-
-
 
     render() {
         if (this.state.loading) {
@@ -37,21 +42,31 @@ export default class FetchTeacherSchedule extends React.Component {
             <div>
                 <h3>{this.state.person.full_name}</h3>
                 <div>Расписание с {this.state.week.date_start} по {this.state.week.date_end}</div>
-
                 <div>{this.state.person.id}</div>
                 <div>{this.state.person.full_name}</div>
                 <div>{this.state.person.chair}</div>
-
-                {this.state.schedule.map((schedule) => (
-                    <div className="card">
+                {this.state.schedule.map((schedule, i) => (
+                    <div key={i} className="week__card">
                         <div className="card-body">
-                            <h5 className="card-title">{schedule.weekday}</h5>
+                            <h5  className="card-title">{schedule.weekday}</h5>
                             <h5 className="card-title">{schedule.date}</h5>
-                            <h5>{this.state.schedule[0].lessons[0].subject}</h5>
-                            <h5>{this.state.schedule[0].lessons[0].typeObj.name}</h5>
-                            <h5>{this.state.schedule[0].lessons[0].additional_info}</h5>
-                            <h5>{this.state.schedule[0].lessons[0].time_start} - {this.state.schedule[0].lessons[0].time_end}</h5>
                             <h5>{this.state.schedule[0].lessons[0].auditories[0].building.name}, ауд. {this.state.schedule[0].lessons[0].auditories[0].name}</h5>
+
+
+                            {this.state.schedule[i].lessons.map(function(lessons, i) {
+                                return (
+                                    <span key={i} className="lesson__group">
+                                        <ul className="lessons__list">
+                                            <li className="lesson__item">
+                                                <h5>{lessons.time_start} - {lessons.time_end}</h5>
+                                                <p>{lessons.subject}</p>
+                                                <p>{lessons.typeObj.name}</p>
+                                                <p>{lessons.additional_info}</p>
+                                            </li>
+                                        </ul>
+                                    </span>
+                                )
+                            })}
 
 
 
