adebebcdcb3b61774c6fb80fb3076e2137759c9f
abudimirov <alexys077@gmail.com>
2019-11-08 15:33:06 +0300

    removed FetchTeacherSched and changed structure even further

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import './App.css';
-import FetchTeacherSchedule from "./components/FetchTeacherSchedule";
+import {Header} from "./components/Header";
+import {Week} from "./components/Week";
 
 const teacher_id = "2559";
 
 export default class App extends React.Component {
+    state = {
+        loading: true,
+        person: null
+    };
+
+
+    async componentDidMount() {
+
+        const url = `https://ruz.spbstu.ru/api/v1/ruz/teachers/${teacher_id}/scheduler`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({
+            schedule: data.days,
+            person: data.teacher,
+            week: data.week,
+            days: data.days,
+            loading: false
+        });
+    }
     render() {
+
+        if (this.state.loading) {
+            return  <h3>Загрузка...</h3>;
+        }
+
+        if (!this.state.person) {
+            return  <h3>Нет данных</h3>;
+        }
+
         return (
-            <div className="App">
-                <FetchTeacherSchedule/>
+            <div>
+                <Header name={this.state.person.full_name}
+                        date_start={this.state.week.date_start}
+                        date_end={this.state.week.date_end}
+                        is_odd={this.state.week.is_odd}/>
+                {this.state.schedule.length ? <Week schedule={this.state.schedule}/> : <h3>На эту неделю занятия не поставлены</h3>}
             </div>
         );
     }
 }
-
-
-export { teacher_id };
